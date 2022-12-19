6d67c82c399cdab988e8c504ab40ebd2361175ab
shahin-446 <84090319+shahin-446@users.noreply.github.com>
2022-12-19 17:11:25 +0530

    Update if else.py

--- if else.py
+++ if else.py
@@ -1,5 +1,15 @@
+#simple if else
 a = int(input())
 if a > 18:
   print("Adult")
 else:
   print("Not an adult")
+
+  
+  #even or odd
+  
+  b = int(input())
+  if b%2 == 0:
+    print("Even")
+  else:
+    print("Odd")
